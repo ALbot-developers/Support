708786b9c5200e583c9ec5fe56fc6d1f4cc94857
冬優 <61175422+Huyu2239@users.noreply.github.com>
2021-09-01 14:24:46 +0000

    add: スレッド作成機能

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from libs import ThreadPanelView
+
 
 class SupportBot(commands.Bot):
     def __init__(self, command_prefix):
@@ -14,13 +16,28 @@ class SupportBot(commands.Bot):
                 everyone=False, roles=False, users=True
             ),
         )
-        self.remove_command('help')
+        self.remove_command("help")
+        self.restarted = False
 
     async def on_ready(self):
-        return
+        if self.restarted:
+            return
+        self.restarted = True
+        await self.thread_init()
+        print("ready")
+
+    async def thread_init(self):
+        root_channel = self.get_channel(int(os.environ["THREAD_ROOT_CHANNEL_ID"]))
+        async for message in root_channel.history():
+            if message.author.id != self.user.id:
+                continue
+            await message.delete()
+        view = await ThreadPanelView(self, root_channel).setup()
+        await root_channel.send(**view.build())
 
 
 if __name__ == "__main__":
+    print("starting")
     load_dotenv()
     bot = SupportBot(command_prefix="T.")
     bot.run(os.environ["TOKEN"])
