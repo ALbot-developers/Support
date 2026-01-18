4f0dbebb51dfb09351a78b93cb60f17495dbfc2e
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2026-01-18 16:31:02 +0900

    add: お問い合わせの開始時の埋め込みのデザインを修正

--- commands/setup.js
+++ commands/setup.js
@@ -5,9 +5,10 @@ const {
 	ButtonStyle,
 	MessageFlags,
 	ChannelType,
-	EmbedBuilder,
 	PermissionFlagsBits,
 	InteractionContextType,
+	ContainerBuilder,
+	TextDisplayBuilder,
 } = require('discord.js');
 
 module.exports = {
@@ -20,10 +21,10 @@ module.exports = {
 			option
 				.setName('channel')
 				.setDescription(
-					'お客様が、問い合わせの開始するために使用するチャンネルを指定してください。'
+					'お客様が、問い合わせの開始するために使用するチャンネルを指定してください。',
 				)
 				.addChannelTypes(ChannelType.GuildText)
-				.setRequired(true)
+				.setRequired(true),
 		),
 
 	run: async (client, interaction) => {
@@ -40,32 +41,48 @@ module.exports = {
 				});
 			}
 
-			// embedを作成
-			const embed = new EmbedBuilder()
-				.setTitle('🎫お問い合わせ🎫')
-				.setDescription(
-					'質問・要望・バグ報告がある場合は下のボタンを押してください。\nサポートチームが対応いたします。'
-				)
-				.setColor(0x00eaff)
-				.setFooter({ text: '↓ここをクリックして始めてください↓' });
+			// containerを作成
+			const container = new ContainerBuilder()
+				.addTextDisplayComponents([
+					new TextDisplayBuilder({ content: '# 📪 お問い合わせ' }),
+					new TextDisplayBuilder().setContent(
+						'質問・要望・バグ報告などがある場合は下のボタンを押してください。\nサポートチームが対応いたします。',
+					),
+					new TextDisplayBuilder().setContent(
+						'↓ここをクリックして始めてください↓',
+					),
+				])
+				.setAccentColor(0xffffff);
 
 			// buttonを作成
 			const button = new ActionRowBuilder().addComponents(
 				new ButtonBuilder()
 					.setCustomId('support')
-					.setLabel('お問い合わせを始める')
-					.setStyle(ButtonStyle.Primary)
-					.setEmoji('🎫')
+					.setLabel('問い合わせを開始する')
+					.setStyle(ButtonStyle.Success)
+					.setEmoji('🎫'),
 			);
 
-			//embedとbutton送信
+			//containerとbutton送信
 			const contactUsChannel = interaction.options.getChannel('channel');
 			await contactUsChannel.send({
-				embeds: [embed],
-				components: [button],
+				components: [container, button],
+				flags: MessageFlags.IsComponentsV2,
+			});
+
+			// 成功メッセージを返す
+			await interaction.reply({
+				content: `セットアップが完了しました！${contactUsChannel}にメッセージを送信しました。`,
+				flags: MessageFlags.Ephemeral,
 			});
 		} catch (err) {
-			console.log(err);
+			console.error(err);
+			await interaction
+				.reply({
+					content: 'セットアップ中にエラーが発生しました。',
+					flags: MessageFlags.Ephemeral,
+				})
+				.catch(() => {});
 		}
 	},
 };
